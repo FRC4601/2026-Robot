97955b83ed6d8be83384da7aa0a72f5dd1205753
Canfield Circuit Birds Team-4601 <60019160+FRC4601@users.noreply.github.com>
2026-02-03 17:33:04 -0500

    Update Intake.java
    instance variables for intake and part of the constructive

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,15 +1,32 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
+//import edu.wpi.first.wpilibj2.command.Command;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//import edu.wpi.first.wpilibj.DigitalInput; (used for bottom limit on 2025's elevator)
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+//import com.revrobotics.spark.SparkClosedLoopController;
+//import com.revrobotics.spark.ClosedLoopSlot;
+//import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.ResetMode;
+//import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {}
+
+  private final SparkMax intakeMotor;
+  private final SparkMaxConfig intakeConfig;
+  
+  public Intake() {
+
+    intakeMotor = new SparkMax(0, MotorType.kBrushless); // 0 is just a placeholder
+    
+    
+  }
 
   /**
    * Example command factory method.

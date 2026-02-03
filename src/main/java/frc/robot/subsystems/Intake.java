775b178567f34b9081056b5c8bb6472c37d7e6ce
Canfield Circuit Birds Team-4601 <60019160+FRC4601@users.noreply.github.com>
2026-02-03 17:40:21 -0500

    Update Intake.java
    added basic functionalities and methods to the class

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -24,32 +24,13 @@ public class Intake extends SubsystemBase {
   public Intake() {
 
     intakeMotor = new SparkMax(0, MotorType.kBrushless); // 0 is just a placeholder
-    
-    
-  }
+    intakeConfig = new SparkMaxConfig();
+    leaderConfig.idleMode(IdleMode.kBrake)
+        //.encoder.positionConversionFactor(2.2*OperatorConstants.inchesPerMotorRotation);
+        // ^ same issue as in the arm subsystem
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+    intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    
   }
 
   @Override
@@ -57,8 +38,17 @@ public class Intake extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+  public void runIntake(double speed) {
+    intakeMotor.set(speed);
+    // might have to make this negative depending on how the motors are placed
   }
+
+  public void stopIntake() {
+    intakeMotor.set(0);
+  }
+
+  //public void updateDashboard() {}
+  // ^ might create this in the future if we add more stuff, but as of now, we don't need it
+  
+
 }

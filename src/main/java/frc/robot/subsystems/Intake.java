6e3d731d68548dbaead1f0aacdda5cf23d2cc49e
Zack <zmashqbeh@gmail.com>
2026-02-05 18:14:37 -0500

    Clean up Intake.java by removing comments and imports
    Removed unused imports and commented-out code.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,18 +1,11 @@
 package frc.robot.subsystems;
-
-//import edu.wpi.first.wpilibj2.command.Command;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import edu.wpi.first.wpilibj.DigitalInput; (used for bottom limit on 2025's elevator)
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
-//import com.revrobotics.spark.SparkClosedLoopController;
-//import com.revrobotics.spark.ClosedLoopSlot;
-//import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.ResetMode;
-//import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import frc.robot.Constants.IntakeConstants;

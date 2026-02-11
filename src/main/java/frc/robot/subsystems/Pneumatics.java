accd89e291740f7723e5d889dae4212094ba6646
deluciadominic-maker <delucia.dominic@student.canfieldschools.net>
2026-02-11 17:30:44 -0500

    Update Pneumatics.java
    jk

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -1,8 +1,9 @@
 // hi idk what i'm doing
 
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Pneumatics extends SubsystemBase {
 	// this makes a new class for pneumatics
@@ -12,4 +13,5 @@ public class Pneumatics extends SubsystemBase {
 		pneumatics = new DoubleSolenoid(PneumaticsModuleType.REVPH, forwardChannel: 0, reverseChannel: 0);
 		// we might need to change the port numbers later idk which one's we're gonna use
 	}
+
 }

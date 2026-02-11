064ae17f483b75fff1e25436ce97fae4cdcd0c19
Zack <zmashqbeh@gmail.com>
2026-02-11 18:30:55 -0500

    Add Hopper subsystem with DoubleSolenoid control
    made the basics of the subsystem. effectively copy + pasted from 2023. NEED to actually research what it does when I get time but this is fine for now

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -1,1 +1,35 @@
+// will have ONE piston (MAYBE two in the future)
 
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+// ^ no idea what module type does but it was in the 2023 code
+// import edu.wpi.first.wpilibj.DoubleSolenoid.Value; (this might be needed)
+
+public class Hopper extends SubsystemBase {
+
+  public final DoubleSolenoid piston;
+
+  public Hopper() {
+    piston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    // ^ still need to research the double solenoid class
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void openHopper(){
+    piston.set(Value.kForward);
+    // ^ NO CLUE what this does rn. will figure out tomorrow (thursday)
+  }
+  
+  public void closeHopper(){
+    piston.set(Value.kReverse);
+    // ^ see above
+  }
+  
+}

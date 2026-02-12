9a47fce89aea0b39cac932f8cfac6bdf72307e23
Zack <zmashqbeh@gmail.com>
2026-02-12 16:42:05 -0500

    Finished Hopper subsystem?
    I think I'm done but I will check on VSCode to look for any errors later (cannot today because of PT-conferences)

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -6,13 +6,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 // ^ differentiates between whether we have a CTREPCM pneumatics type or a REVPH. I need to ask donald which we have.
+import frc.robot.Constants.PneumaticsConstants;
 
 public class Hopper extends SubsystemBase {
 
   public final DoubleSolenoid piston;
 
   public Hopper() {
-    piston = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
+    piston = new DoubleSolenoid(PneumaticsModuleType.REVPH, 
+                                PneumaticsConstants.HOPPER_FORWARD_ID, PneumaticsConstants.HOPPER_REVERSE_ID);
   }
 
   @Override
@@ -20,14 +22,19 @@ public class Hopper extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  public void openHopper(){
+  public void openHopper() {
     piston.set(DoubleSolenoid.Value.kForward);
-    // ^ NO CLUE what this does rn. will figure out tomorrow (thursday)
+    // the names MIGHT be backwards. idk if forward is open or close
   }
   
-  public void closeHopper(){
-    piston.set(Value.kReverse);
-    // ^ see above
+  public void closeHopper() {
+    piston.set(DoubleSolenoid.Value.kReverse);
+  }
+
+  public void stopHopper() {
+    piston.set(DoubleSolenoid.Value.kOff);
+    // no clue if we will need to stop it when it's fully out/fully in or if it's done automatically
+    // will test eventually
   }
   
 }

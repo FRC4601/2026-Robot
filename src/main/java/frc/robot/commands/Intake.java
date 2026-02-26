efbfe66344bcf19f8222d994d6c2f519af5d8a8d
Zack <zmashqbeh@gmail.com>
2026-02-26 17:59:47 -0500

    Update RunStager command with new subsystem dependencies
    Refactor RunStager command to include Hopper, Arm, and Intake dependencies.

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -2,31 +2,33 @@ package frc.robot.commands;
 import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Stager;
+import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Intake;
 
 
+public class Intake extends Command {
 
-public class RunStager extends Command {
+    private final Hopper hopper;
+    private final Arm arm;
+    private final intake Intake;
 
-    private final Stager stager;
-    private final Supplier<Boolean> readyToShoot;
-
-    public RunStager(Stager stager, Supplier<Boolean> readyToShoot) {
-        this.stager = stager;
-        this.readyToShoot = readyToShoot;
-        addRequirements(stager); // Declare subsystem dependencies
+    
+    @Override
+    public void initialize(Hopper hopper, Arm arm, Intake intake) {
+    
+        this.hopper = hopper;
+        this.arm = arm;
+        this.intake = intake;
+            
     }
-@Override
-public void initialize() {
 
-    }
+    @Override
+    public void execute() {
 
-@Override
-public void execute() {
-    if (readyToShoot.get()) {
-        stager.setStagerSpeed(0.5); // Set to desired staging speed (tune as needed)
-    } else {
-        stager.setStagerSpeed(0.0); // Stop stager if not ready to shoot
-    }
+        // i need to make it so the hopper extends but tbh i have no clue how to do this
+
+        // a lot of stuff happened at robotics on thursday so i was unfortunately unable to code a majority of this
+        // hopefully it will be finished/substantially improved on friday
     }
 }

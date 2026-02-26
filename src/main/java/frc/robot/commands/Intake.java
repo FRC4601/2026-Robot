40bbf4a89d48fa498a5f3e57d0a5025883fb9278
Zack <zmashqbeh@gmail.com>
2026-02-26 16:21:05 -0500

    Add RunStager command to control stager speed

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -1,1 +1,32 @@
+package frc.robot.commands;
+import java.util.function.Supplier;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Stager;
+
+
+
+public class RunStager extends Command {
+
+    private final Stager stager;
+    private final Supplier<Boolean> readyToShoot;
+
+    public RunStager(Stager stager, Supplier<Boolean> readyToShoot) {
+        this.stager = stager;
+        this.readyToShoot = readyToShoot;
+        addRequirements(stager); // Declare subsystem dependencies
+    }
+@Override
+public void initialize() {
+
+    }
+
+@Override
+public void execute() {
+    if (readyToShoot.get()) {
+        stager.setStagerSpeed(0.5); // Set to desired staging speed (tune as needed)
+    } else {
+        stager.setStagerSpeed(0.0); // Stop stager if not ready to shoot
+    }
+    }
+}

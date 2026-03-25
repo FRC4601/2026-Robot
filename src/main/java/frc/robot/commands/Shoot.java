068daec3e2d10076b674e4e5cc28ca28e8faa589
Zack <zmashqbeh@gmail.com>
2026-03-25 07:49:29 -0400

    Enhance comments in Shoot.java for clarity
    Added comments to clarify shooting logic and potential issues with RPM.

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -77,6 +77,8 @@ public void execute() {
         double y = pose.getY(); // Get the robot's current position on the field, which can be used for distance-based adjustments to shooting
         double rotation = pose.getRotation().getDegrees(); // Get the robot's current rotation in degrees relative to the field, can be used to aim turret
 
+        // if we give it an rpm it can't reach, what will happen?
+        // it'll probably just max out at the closest possible rpm, but we should maybe test just in case
         shooter.setVelocity(rpm);
         stager.setStagerSpeed(-StagerConstants.STAGER_SPEED);
         agitator.setAgitatorSpeed(-AgitatorConstants.AGITATOR_SPEED);
